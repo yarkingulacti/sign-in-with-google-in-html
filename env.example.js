7203c5bf782b8aea97b58239f52577f54cb7e112
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2025-01-17 08:50:14 +0300

    update env.example.js

--- env.example.js
+++ env.example.js
@@ -1,5 +1,3 @@
-window.env = {
-  GOOGLE_CLIENT_ID: "YOUR_GOOGLE_CLIENT_ID",
-  BACKEND_URL: "https://example.com",
-  BACKEND_OAUTH_VERIFY_URI: "/api/auth/google"
-};
+var GOOGLE_CLIENT_ID = "";
+var BACKEND_URL = "";
+var BACKEND_OAUTH_VERIFY_URI = "";
